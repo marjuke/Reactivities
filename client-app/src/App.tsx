faa7ce766bb711d6e4191179478e03ac642fa748
Marjuke Rahman Razi <marjuke.rahman10@gmail.com>
2022-12-29 13:33:19 +0600

    Client app changes

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
     const [activities, setActivities] = useState([]);
@@ -13,18 +14,15 @@ function App() {
             })
     }, [])
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <ul>
-                    {activities.map((activity:any) => (
-                        <li key={activity.id}>
-                            {activity.title}
-                        </li>
-                    ))}
-                </ul>
-            </header>
-        </div>
+      <div className="App">
+        <Header as="h2" icon="users" content="Reactivities" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <List>
+          {activities.map((activity: any) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </div>
     );
 }
 export default App;
